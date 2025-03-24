93aa58919b5ee7405a01cd8de673e129199f6275
Marcos Paulo <mpaulosp.315@gmail.com>
2025-03-24 13:17:03 -0300

    fix: GameChart type fixed

--- type/games.py
+++ type/games.py
@@ -1,25 +1,5 @@
 from pydantic import BaseModel
 from typing import Optional
-
-# class GameProjection(BaseModel):
-#     id: Optional[int] = 0
-#     img: Optional[int] = None
-#     title: Optional[int] = 1
-#     console: Optional[int] = 1
-#     genre: Optional[int] = 1
-#     publisher: Optional[int] = 1
-#     developer: Optional[int] = 1
-#     critic_score: Optional[int] = 1
-#     total_sales: Optional[int] = 1
-#     na_sales: Optional[int] = 1
-#     jp_sales: Optional[int] = 1
-#     pal_sales: Optional[int] = 1
-#     other_sales: Optional[int] = 1
-#     release_date: Optional[int] = 1
-#     year: Optional[int] = None
-#     month: Optional[int] = None
-#     day: Optional[int] = None
-
 class GameChart(BaseModel):
     title: str
     console: str
