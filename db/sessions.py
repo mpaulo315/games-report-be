75473c0c284098a72b9a9ac31978d7f38966e6cd
Marcos Paulo <mpaulosp.315@gmail.com>
2025-03-24 13:17:56 -0300

    fix: .env related corrections on db files

--- db/sessions.py
+++ db/sessions.py
@@ -1,24 +1,20 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from os import getenv
+
 load_dotenv()
 
-from .classes import ReadOnlyCollection
-
-URI = getenv('CLUSTER_URI')
 DATABASE = getenv('DATABASE')
-DEFAULT_COLLECTION = "games"
+COLLECTION = getenv("COLLECTION")
     
-def get_database_conn():
-    return MongoClient(URI)[DATABASE]
-
 def get_session():
-    with MongoClient(URI) as client:
-        client = MongoClient(URI)
-        return client[DATABASE][DEFAULT_COLLECTION]
-        # return ReadOnlyCollection(client[DATABASE][DEFAULT_COLLECTION])
+    QUERY_URI = getenv('QUERY_URI')
+    with MongoClient(QUERY_URI) as client:
+        client = MongoClient(QUERY_URI)
+        return client[DATABASE][COLLECTION]
 
 def get_migration_session():
-    with MongoClient(URI) as client:
-        client = MongoClient(URI)
-        return client[DATABASE][DEFAULT_COLLECTION]
+    MIGRATION_URI = getenv('MIGRATION_URI')
+    with MongoClient(MIGRATION_URI) as client:
+        client = MongoClient(MIGRATION_URI)
+        return client[DATABASE][COLLECTION]
